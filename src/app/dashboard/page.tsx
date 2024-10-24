517c22581725b1b2ea0fc9259eda8422439c4e0d
tuzolanacalueto <tuzolanacalueto@gmail.com>
2024-10-24 14:00:31 +0100

    mais um commit

---
+++ src/app/dashboard/page.tsx
@@ -0,0 +1,34 @@
+import { Orders } from "./components/orders";
+import { api } from '@/services/api'
+import { getCookieServer } from '@/lib/cookieServer'
+import { OrderProps } from '@/lib/order.type'
+
+async function getOrders(): Promise<OrderProps[] | []>{
+  try{
+    const token = getCookieServer();
+
+    const response = await api.get("/orders", {
+      headers: {
+        Authorization: `Bearer ${token}`
+      }
+    })
+
+    return response.data || []
+
+  }catch(err){
+    console.log(err);
+    return [];
+  }
+}
+
+
+export default async function Dashboard(){
+
+  const orders = await getOrders();
+
+  return(
+    <>
+     <Orders orders={orders}/>
+    </>
+  )
+}
